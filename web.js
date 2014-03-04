b4cfa8a93ac267ff7862cf529d611b0f249306c0
Lucas Churchill <lucas@agtlucas.com>
2014-03-03 22:04:03 -0300

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,19 @@
 var views = require('co-views');
+var koa = require('koa');
+var app = module.exports = koa();
+
+var render = views(__dirname + '/views', { ext: 'ejs' });
+
+var user = {
+  name: {
+    first: 'Lucas',
+    last: 'Churchill'
+  },
+  age: 22
+};
+
+app.use(function *() {
+  this.body = yield render('user', { user: user });
+});
+
+if (!module.parent) app.listen(4000);
